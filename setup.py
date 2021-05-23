833192e5fcce490f121298ff240248629f81b7c3
iamredbar <brendanjensenrecordings@gmail.com>
2021-05-22 20:37:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 import pathlib
-
+import platform
 from setuptools import find_packages, setup
 
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+if platform.system() == 'Windows':
+    I_R = [
+        'kivy[full,sdl2,glew]',
+        'kivymd',
+        'bitshares',
+        'pypubsub'
+    ]
+else:
+    I_R = [
+        'kivy',
+        'kivymd',
+        'bitshares',
+        'pypubsub'
+    ]
+
 setup(
     name='PoolTool',
-    version='2.0.5',
+    version='2.0.6',
     description='A simple Python tool to help anyone use Liquidity Pools on the BitShares blockchain.',
     long_description=README,
     long_description_content_type='text/markdown',
@@ -17,12 +32,7 @@ setup(
     license='MIT',
     url='https://github.com/iamredbar/PoolTool',
     packages=find_packages(),
-    install_requires=[
-        'kivy[base]',
-        'kivymd',
-        'bitshares',
-        'pypubsub'
-    ],
+    install_requires=I_R,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
